52da7110ee48ffac625926097821d4ec344e041f
kylekylekylekyle <31939134+kylekylekylekyle@users.noreply.github.com>
2018-11-05 16:53:04 -0500

    changed implementation for ioctl + read/write

--- encryptor_app.c
+++ encryptor_app.c
@@ -13,28 +13,29 @@
 
 #define CRYPT_IOC_MAGIC (0xAA)
 
-typedef struct ioctl_input {
+typedef struct configure_input {
 	int index;
 	char key[33];
-	char buffer[257];
-} ioctl_input;
+} configure_input;
+
 
 #define CRYPT_CREATE _IO(CRYPT_IOC_MAGIC, 0)
-#define CRYPT_DESTROY _IOR(CRYPT_IOC_MAGIC, 1, ioctl_input)
-#define CRYPT_CONFIGURE _IOR(CRYPT_IOC_MAGIC, 2, ioctl_input)
-#define CRYPT_WRITE _IOWR(CRYPT_IOC_MAGIC, 3, ioctl_input)
-#define CRYPT_READ _IOWR(CRYPT_IOC_MAGIC, 4, ioctl_input)
+#define CRYPT_DESTROY _IOR(CRYPT_IOC_MAGIC, 1, int)
+#define CRYPT_CONFIGURE _IOR(CRYPT_IOC_MAGIC, 2, configure_input)
 
 int main() {
 	int fd;
+	int encrypt_fd;
+	int decrypt_fd;
 	int loop = 1;
 	int num_pairs = 0;
 	int index;
+	int retval;
 	char key[33];
 	char buffer[257];
 	char type; //C - Create D - Destroy O - cOnfigure R - Read W - Write
 
-	ioctl_input message = {0, "", ""};
+	configure_input config = {0, ""};
 
 	fd = open("/dev/cryptctl", O_RDWR);
 	if (fd < 0) {
@@ -62,86 +63,128 @@ int main() {
 				else {
 					printf("Enter the index you want destroyed (0-9):\n");
 					scanf("%d", &index);
-					if (index + 1 > num_pairs) {
-						printf("Index doesn't exist, please try again with a lower number\n");
-					}
-					else if (index < 0) {
-						printf("Negative index, please try again with a positive number\n");
+					if (index < 0 || index > 10) {
+						printf("Index is out of bounds\n");
 					}
 					else {
-						num_pairs -= 1;
-						message.index = index;
-						ioctl(fd, CRYPT_DESTROY, (ioctl_input*) &message);
+						retval = ioctl(fd, CRYPT_DESTROY, &index);
+						if (retval < 0) {
+							printf("Index doesn't exist\n");
+						}
+						else {
+							num_pairs -= 1;
+						}
 					}
 				}
 				break;
 			case 'O':
 				if (num_pairs <= 0) {
-					printf("No pairs to configure, please create (C) a pair\n");
+					printf("No pairs to destroy, please create (C) a pair\n");
 				} 
 				else {
-					printf("Enter the index you want configured (0-9):\n");
+					printf("Enter the index you want destroyed (0-9):\n");
 					scanf("%d", &index);
-					if (index + 1 > num_pairs) {
-						printf("Index doesn't exist, please try again with a lower number\n");
-					}
-					else if (index < 0) {
-						printf("Negative index, please try again with a positive number\n");
+					if (index < 0 || index > 10) {
+						printf("Index is out of bounds\n");
 					}
 					else {
 						printf("Enter the key to configure (32 char max):\n");
 						fgets(key, 33, stdin);
-						message.key = key;
-						message.index = index;
-						ioctl(fd, CRYPT_CONFIGURE, (ioctl_input*) &message);
+						strcpy(config.key, key);
+						config.index = index;
+						retval = ioctl(fd, CRYPT_DESTROY, &config);
+						if (retval < 0) {
+							printf("Index doesn't exist\n");
+						}
 					}
 				}
 				break;
 			case 'R':
 				if (num_pairs <= 0) {
-					printf("No pairs to read, please create (C) a pair\n");
+					printf("No pairs to read from, please create (C) a pair\n");
 				} 
 				else {
-					printf("Enter the index you want to read (0-9):\n");
+					printf("Enter the index you want to read from (0-9):\n");
 					scanf("%d", &index);
-					if (index + 1 > num_pairs) {
-						printf("Index doesn't exist, please try again with a lower number\n");
-					}
-					else if (index < 0) {
-						printf("Negative index, please try again with a positive number\n");
-					}
-					else {
-						printf("Enter the message to read (256 char max):\n");
-						fgets(buffer, 257, stdin);
-						message.buffer = buffer;
-						message.index = index;
-						ioctl(fd, CRYPT_READ, (ioctl_input*) &message);
-						buffer = ioctl(fd, CRYPT_READ);
-						printf("%s\n", buffer);
+					printf("Enter the message (256 char max):\n");
+					fgets(buffer, 257, stdin);
+					switch(index) {
+						case 0:
+						
+							break;
+						case 1:
+							
+							break;
+						case 2:
+							
+							break;
+						case 3:
+							
+							break;
+						case 4:
+							
+							break;
+						case 5:
+							
+							break;
+						case 6:
+							
+							break;
+						case 7:
+							
+							break;
+						case 8:
+							
+							break;
+						case 9:
+							
+							break;
+						default:
+							printf("Index out of bounds, please enter an index from 0-9")
 					}
 				}
 				break;
 			case 'W':
 				if (num_pairs <= 0) {
-					printf("No pairs to write, please create (C) a pair\n");
+					printf("No pairs to write to, please create (C) a pair\n");
 				} 
 				else {
-					printf("Enter the index you want to write (0-9):\n");
+					printf("Enter the index you want to write to (0-9):\n");
 					scanf("%d", &index);
-					if (index + 1 > num_pairs) {
-						printf("Index doesn't exist, please try again with a lower number\n");
-					}
-					else if (index < 0) {
-						printf("Negative index, please try again with a positive number\n");
-					}
-					else {
-						printf("Enter the message to read (256 char max):\n");
-						fgets(buffer, 257, stdin);
-						message.buffer = buffer;
-						message.index = index;
-						ioctl(fd, CRYPT_WRITE, (ioctl_input*) &message);
-						buffer = ioctl(fd, CRYPT_WRITE);
-						printf("%s\n", buffer);
+					printf("Enter the message(256 char max):\n");
+					fgets(buffer, 257, stdin);
+					switch(index) {
+						case 0:
+							break;
+						case 1:
+							
+							break;
+						case 2:
+							
+							break;
+						case 3:
+							
+							break;
+						case 4:
+							
+							break;
+						case 5:
+							
+							break;
+						case 6:
+							
+							break;
+						case 7:
+							
+							break;
+						case 8:
+							
+							break;
+						case 9:
+							
+							break;
+						default:
+							printf("Index out of bounds, please enter an index from 0-9")
 					}
 				}
 				break;
@@ -153,6 +196,6 @@ int main() {
 		}
 
 	}
-
+	close(fd);
 	return 0;
 }
